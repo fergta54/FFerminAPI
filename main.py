cf4115460e11c00ae6895b9cc9f20736f80f0c06
fermin becerra <95020613+fergta54@users.noreply.github.com>
2024-06-15 04:43:22 -0400

    Update main.py
    Función Importante

--- main.py
+++ main.py
@@ -43,6 +43,23 @@ def semana():
     res = chat_completion.choices[0].message.content
     return res
 
+@app.route("/var/<data>")
+def varD(data):
+    client = Groq(
+    api_key="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",
+    )
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": data,
+            }
+        ],
+        model="llama3-8b-8192",
+    )
+    res = chat_completion.choices[0].message.content
+    return res
+
 
 
 if __name__ == "__main__":
