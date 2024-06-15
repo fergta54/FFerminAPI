555ff749134dac21de2e625c5f12fa0c50e76783
fermin becerra <95020613+fergta54@users.noreply.github.com>
2024-06-15 04:38:32 -0400

    Update main.py
    ampliación prueba

--- main.py
+++ main.py
@@ -26,6 +26,23 @@ def mes():
     res = chat_completion.choices[0].message.content
     return res
 
+@app.route("/datosSem")
+def mes():
+    client = Groq(
+    api_key="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",
+    )
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": "Solo dame un ejemplo de una tabla de my sql nada mas gracias.",
+            }
+        ],
+        model="llama3-8b-8192",
+    )
+    res = chat_completion.choices[0].message.content
+    return res
+
 
 
 if __name__ == "__main__":
